1ff16f8d7d2d9779e0320e1a79f3a1e78157e43b
jacquelinesayre <code.blooded.belter@gmail.com>
2025-09-05 11:50:39 -0700

    updating avatar

--- client/src/Components/Avatar.jsx
+++ client/src/Components/Avatar.jsx
@@ -1,12 +1,16 @@
 import chewie from "../assets/baby_chewie.jpeg";
+import selfie from "../assets/selfPic.jpg";
 import Orb from "../Orb"
 
 const Avatar = () => {
   return (
     <>
+    <div className="bg-black w-20 h-20 ml-12 mt-10 p-1 rounded-full absolute">
+          </div>
+
       <img
-        className="w-20 h-20 ml-12 mt-10 p-1 rounded-full  absolute"
-        src={chewie}
+        className="w-20 h-20 ml-12 mt-10 p-1 rounded-full absolute"
+        src={selfie}
         alt="Bordered avatar"
       />
       {/* Testing avatar orb thingy  */}
