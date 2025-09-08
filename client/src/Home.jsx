f7078895014b4b2ffa1e3cb26a9eaf5965633036
jacquelinesayre <code.blooded.belter@gmail.com>
2025-09-08 13:19:42 -0700

    Found resume bug

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -27,7 +27,7 @@ const Home = ({ showDownload, setShowDownload }) => {
           <div class="flex flex-col w-full max-w-[220px] leading-1.5 p-4 border-gray-200 bg-gray-100 rounded-e-xl rounded-es-xl  dark:bg-red-200">
             <div class="flex items-center space-x-2 rtl:space-x-reverse">
               <div className="mx-auto">
-                <a href={ResumeDM} download>
+                <a href={Resume} download>
                   <button
                     type="button"
                     className="z-50 mt-4 hoverme inline-flex items-center text-white bg-gradient-to-r from-teal-400 via-teal-500 to-teal-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-teal-300 focus:ring-teal-800 shadow-lg shadow-teal-500/50 shadow-lg shadow-teal-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
